7e10335b013b7d9f736860fb510e81a68fad1eea
Nico Bokhari <nebokha@umich.edu>
2024-03-26 12:54:46 -0400

    create dictionary structure to hold device information during algo

--- prototype/src/client/algo_utils.py
+++ prototype/src/client/algo_utils.py
@@ -1,19 +1,61 @@
 import math
 
 class Processing_Unit:
-    def __init__(self, macAddr, publishing_set, capacity):
+    def __init__(self, macAddr:str, publishing_set:list, capacity:float):
         self._mac = macAddr
         self._freqs = publishing_set
         self._battery = capacity
+        self._freq_min = min(self._freqs)
+        self._numExecutions = 0
+        self._OBSERVATION_PERIOD = 60
+        self._ENERGY_PER_EXECUTION = 10
     
     def changeInExecutions(self, newExecutions):
         return newExecutions - self._numExecutions
     
-    def calculateExecutions(self):
-        # only calculate number of executions if there is at least 1 item in _freqs
-
+    def calculateExecutions(self, newTask = None):
+        if newTask: 
+            # if there is a new task, add it to freqs, change the min if necessary
+            self._freqs.append(newTask)
+            self._freq_min = min(self._freqs)
+        numExecutions = 0
         # threshold for execution interval MAY CHANGE
-        threshold = math.ceil(min(self._freqs) / 2) - 1
+        threshold = math.ceil(self._freq_min / 2) - 1
+        # list that will hold removals
+        removes = list()
+        # copy of freqs
+        freqCopy = self._freqs
+        # remove the min for these calculations
+        freqCopy.remove(self._freq_min)
+        # Remove the numbers that are within a threshold of a multiple of the minimum frequency
+        removes = [freq for freq in freqCopy if (freq % self._freq_min < threshold) or ((self._freq_min - freq % self._freq_min) < threshold)]
+        # Remove the numbers from freqCopy
+        freqCopy = [freq for freq in freqCopy if freqCopy not in removes]
+        # place minimum back in
+        freqCopy.append(self._freq_min)
+        for i in range(len(freqCopy)):
+            numExecutions += math.ceil(self._OBSERVATION_PERIOD / freqCopy[i])
+        return numExecutions
+        
+class Devices:
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+    
+    def __init__(self) -> None:
+        # key = macAddr, value = Processing_Unit
+        self._units = dict()
+
+    def addProcessingUnit(self, newUnit:Processing_Unit):
+        newUnit._numExecutions = newUnit.calculateExecutions()
+        self._units[newUnit._mac] = newUnit
+
+        
+
+
 
 
 
--- temp/listManip.py
+++ temp/listManip.py
@@ -1,18 +1,19 @@
 import math
-import time
-import random
-nums = [random.randint(1, 100) for i in range(10)]
+# nums = [random.randint(1, 100) for i in range(10)]
+nums = [10, 12, 20, 37, 35, 45]
+print(nums)
 nums_min = min(nums)
 threshold = math.ceil(nums_min / 2) - 1
 print(f"threshold = {threshold}")
-start = time.time()
 # Create a list of numbers to remove
 removes = [num for num in nums if (num % nums_min < threshold) or ((nums_min - num % nums_min) < threshold)]
 # Remove the numbers from the original list
 nums = [num for num in nums if num not in removes]
-stop = time.time()
-print(f"duration: {stop - start}")
-# Append the minimum number to the list
 nums.append(nums_min)
+numExecutions = 0
+for i in range(len(nums)):
+    numExecutions += math.floor(60 / nums[i])
+    print(f"executions now at: {numExecutions}")
 print(nums_min)
 print(nums)
+print(numExecutions)
