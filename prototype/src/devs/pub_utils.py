3528ba3427702859dc13a25cad838a6ccec05694
Nico Bokhari <nebokha@umich.edu>
2024-04-06 20:50:16 -0400

    track executions and energy in pub_utils

--- prototype/src/devs/pub_utils.py
+++ prototype/src/devs/pub_utils.py
@@ -18,9 +18,10 @@ class PublisherUtils:
         self._got_cmd = None # set to true and mqtt awaits self. to be set to False after msg is received
         self._end_round = None # 
         self._publishes = None
+        self._current_executions = 0
         
 
-    def setParameters(self, username, password, Mac_addr, start_battery):
+    def setParameters(self, username, password, Mac_addr, start_battery, in_sim, energy_per_execution):
         # Set Attributes to Parameters
         self._USERNAME = username
         self._PASSWORD = password
@@ -28,10 +29,20 @@ class PublisherUtils:
         self._deviceMac = Mac_addr # mac address of IoT device 
         self._battery = float(start_battery)
         self._CMD_TOPIC = "sensor/cmd/" + self._deviceMac # where IoT receives command on where to publish
+        self._IN_SIM = in_sim
+        self._ENERGY_PER_EXECUTION = energy_per_execution
 
     def setPublishing(self, pub_cmd:dict):
         self._publishes = pub_cmd
 
-    # TODO: Need to store variable that tracks whether we are in simulation or experiment
-    # TODO: Variable to track number of executions currently running
-    # TODO: Function to decrease battery by some constant per execution (same constant userd by prototype to generate assignments)
+    def decreaseSimEnergy(self):
+        energyUsed = self._current_executions * self._ENERGY_PER_EXECUTION
+        if self._battery > 0 and self._battery > energyUsed: 
+            self._battery = self._battery - energyUsed
+            return True
+        else: 
+            self._battery = 0
+            return False
+
+    def saveNewExecutions(self, newExecutions):
+        self._current_executions = newExecutions
