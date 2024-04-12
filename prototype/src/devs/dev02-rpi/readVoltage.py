98af230881408955fa42dec60d89e5c9a309b3fb
Nico <nebokha@umich.edu>
2024-04-12 13:33:01 -0400

    finished error adjustment with average

--- prototype/src/devs/dev02-rpi/readVoltage.py
+++ prototype/src/devs/dev02-rpi/readVoltage.py
@@ -1,13 +1,14 @@
 from gpiozero import MCP3008
 from time import sleep
 
-# Define which channel you are using on the MCP3008
-#channel = 0  # Example: Channel 0
-
+# Define which channel you are using on the MCP3
 # Create an MCP3008 object, specifying the SPI port and device
 adcChannelZero = MCP3008(channel=0)
 adcChannelSeven = MCP3008(channel=7)
-
+ADD_CONSTANT  = 0.6
+CH_7_DIVISION_CONSTANT = (1.0526 + 0.9524)/2
+CH_0_DIVISION_CONSTANT = (0.4524 + 0.5526)/2
+STUNT_RESISTOR_OHMS = 1000
 try:
     while True:
         # Read the analog value from the ADC
@@ -18,16 +19,16 @@ try:
 
         
         # Optional: Convert the analog alue to voltage (assuming 3.3V reference)
-        cZeroVoltage = cZeroAnalog  * 5 + 0.6 
-        cSevenVoltage = cSevenAnalog * 5 / 1000 
-        print(f"Battery Current is between: {cSevenVoltage / 1.0526} and {cSevenVoltage / 0.9524}")
+        cZeroVoltage = cZeroAnalog  * 5 + ADD_CONSTANT
+        cSevenVoltage = cSevenAnalog * 5 / STUNT_RESISTOR_OHMS 
+        print(f"Battery Current is {cSevenVoltage / CH_7_DIVISION_CONSTANT}")
         print(f"1/2 Battery Voltage: {cZeroVoltage}")
-        print(f"Full Battery Voltage is between: {cZeroVoltage / 0.5526} and {cZeroVoltage / 0.4524}")
+        print(f"Full Battery Voltage is between: {cZeroVoltage / CH_0_DIVISION_CONSTANT}")
 
         print("============")
         
         # Add a small delay before reading again
-        sleep(1)
+        sleep(10)
 
 except KeyboardInterrupt:
     print("Exiting...")
