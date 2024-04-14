1b8af7c2a1264b20f73af61fa68ac587f7034e47
Nico Bokhari <nebokha@umich.edu>
2024-04-14 16:42:04 -0400

    fixed issue with subscribers not running

--- mqtt-alone/devs/run_pubs.py
+++ mqtt-alone/devs/run_pubs.py
@@ -6,8 +6,8 @@ def main():
     pubsfilename = "devices.csv"
     pidfilename = "pub_pidfile.txt"
     try: 
-        with open(pubsfilename, 'r', newline='') as subfile:
-            reader = csv.reader(subfile)
+        with open(pubsfilename, 'r', newline='') as pubfile:
+            reader = csv.reader(pubfile)
             rows = list(reader)
     except FileNotFoundError:
         print("File not found ", pubsfilename)
@@ -18,12 +18,12 @@ def main():
         username = rows[i][1]
         battery = rows[i][2]
         energy = rows[i][3]
-        range = rows[i][4]
+        freq_range = rows[i][4]
         topicList = rows[i][5:len(rows[i])] # rest of rows are the topics
         topicString = ','.join(topicList)
         # append subscriber's command to commands list
         print(username)
-        command.append(["python3", "sensor.py", exp_type, username, battery, energy, range, topicString, "&"])
+        command.append(["python3", "sensor.py", exp_type, username, battery, energy, freq_range, topicString, "&"])
     with open(pidfilename, "a") as file: 
         for sub in command: 
             print(sub)
