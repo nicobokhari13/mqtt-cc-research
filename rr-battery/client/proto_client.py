97683c7a2f3d9ff491b5c3b31347f91187174547
Nico Bokhari <nebokha@umich.edu>
2024-04-15 09:51:07 -0400

    fixed issue with algo handler

--- rr-battery/client/proto_client.py
+++ rr-battery/client/proto_client.py
@@ -48,6 +48,7 @@ import status_handler
     # exp_type, deviceMac, battery, energy_per_execution, freq_range, topic publishings
     # 1         2           3           4                   5           6  -> ...
 def main():
+    utils = ProtoUtils()
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     devicesFile = sys.argv[1]
     in_sim = sys.argv[2]
@@ -75,13 +76,12 @@ def main():
         mac = rows[i][1]
         startBattery = rows[i][2]
         database.addDevice(MAC_ADDR=mac, BATTERY=startBattery)
-        #status_handler.logPublisherMetrics(time=current_time, mac=mac, battery=startBattery, memory_util_perc="None", cpu_util_perc="None", cpu_temp="None")
+        status_handler.logPublisherMetrics(time=current_time, mac=mac, battery=startBattery, memory_util_perc="None", cpu_util_perc="None", cpu_temp="None")
         topicList = rows[i][5:len(rows[i])] # rest of rows are the topics
         for topic in topicList:
             database.addDeviceTopicCapability(MAC_ADDR=mac, TOPIC=topic)
     database.closeDB()
     # create it once
-    utils = ProtoUtils()
     devices = Devices()
     devices.addEnergyPerExecution(energy_per_execution)
     devices.addConcurrencyThreshold(threshold)
